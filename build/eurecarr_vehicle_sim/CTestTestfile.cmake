8b5d85878abb8d0e8be06e1b97eaa4cda3430d93
sanjar-techie <sanjar.techie@gmail.com>
2023-04-13 11:20:34 +0900

    Code cleanup

--- build/eurecarr_vehicle_sim/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/team6/catkin_ws/src/eurecarr_vehicle_sim
-# Build directory: /home/team6/catkin_ws/build/eurecarr_vehicle_sim
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
